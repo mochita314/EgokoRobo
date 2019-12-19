5ff137c4b36328f948f7329e541cce163c1d6451
鬼澤英里 <kizawa-eri@g.ecc.u-tokyo.ac.jp>
2019-12-19 22:35:51 +0900

    1219

--- CNN/code/detect_parts.py
+++ CNN/code/detect_parts.py
@@ -23,7 +23,7 @@ from matplotlib import pylab as plt
 from model import *
 
 parser = argparse.ArgumentParser(description='Predict facial landmark')
-parser.add_argument('testfile', type=str, help='testfile created by imglab tool')
+#parser.add_argument('testfile', type=str, help='testfile created by imglab tool')
 parser.add_argument('model', type=str, help='model file')
 parser.add_argument('--batchsize', '-b', type=int, default=16, help='Number of images in each mini-batch')
 parser.add_argument('--iteration', '-i', type=int, default=1, help='Number of iteration times')
@@ -35,15 +35,21 @@ serializers.load_npz('../model/'+args.model, model)
 data = []
 
 # パーツを検出させたい画像の読み込み
-img = ImageOps.invert(Image.open('../../input_img/ilust.png').convert('L'))
+img = ImageOps.invert(Image.open('../test_data/t04.jpg').convert('L'))
+
+width,height = img.size
+size = min(width,height)
+
+img = img.crop((width//2-size//2,height//2-size//2,width//2+size//2,height//2+size//2))
+img = img.resize((100,100))
 img = np.array(img,dtype=np.float32) / 256.0
 
-data.append({'img':img})
+def 
 
 def mini_batch_data_without_t(input_data):
     img_data = []
     for j in range(args.batchsize):
-        img_data.append(input_data['img'])
+        img_data.append(input_data)
     x = Variable(np.array(img_data, dtype=np.float32))
     x = F.reshape(x, (args.batchsize, 1, imgsize, imgsize))
     return x
@@ -51,11 +57,4 @@ def mini_batch_data_without_t(input_data):
 x = mini_batch_data_without_t(img)
 y = model(x)
 
-for i in range(args.batchsize):
-    show_img_and_landmark(x.data[i][0],y.data[i].reshape((landmark,2)))
-
-
-
-
-
-
+show_img_and_landmark(x.data[0][0],y.data[0].reshape((landmark,2)))
